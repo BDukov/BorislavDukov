78886acfbe7b5e0ae95efe45de0cae908aa18d56
Borislav Dukov <borislav.dukov@icloud.com>
2024-03-31 23:24:25 +0300

    edit header hover effects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
+
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGithub, faLinkedin, faUpwork } from '@fortawesome/free-brands-svg-icons';
 
 import { Link } from "react-router-dom";
 import "./Header.css";
 
+function SocialIcon({ icon }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+      <FontAwesomeIcon 
+          icon={icon} 
+          style={{ fontSize: '2em', color: isHovered ? 'gray' : 'white' }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+      />
+  );
+}
+
 export default function Header() {
+
   return (
     <>
       <div className="header">
@@ -40,27 +56,27 @@ export default function Header() {
 
         <ul className="social-icons">
         <Link to='https://www.facebook.com/profile.php?id=100001198746109' target="_blank">
-            <ul>
-            <FontAwesomeIcon icon={faFacebook} style={{fontSize: '2em', color: 'white'}}/>
-            </ul>
+            <li>
+            <SocialIcon icon={faFacebook}/>
+            </li>
         </Link>
 
         <Link to='https://github.com/BDukov' target="_blank">
-            <ul>
-            <FontAwesomeIcon icon={faGithub} style={{fontSize: '2em', color: 'white'}}/>
-            </ul>
+            <li>
+            <SocialIcon icon={faGithub}/>
+            </li>
         </Link>
 
         <Link to='https://www.linkedin.com/in/borislav-dukov/' target="_blank">
-            <ul>
-            <FontAwesomeIcon icon={faLinkedin} style={{fontSize: '2em', color: 'white'}}/>
-            </ul>
+            <li>
+            <SocialIcon icon={faLinkedin}/>
+            </li>
         </Link>
         
         <Link to='https://www.upwork.com/freelancers/~011ab4f1b065de0c89' target="_blank">
-            <ul>
-            <FontAwesomeIcon icon={faUpwork} style={{fontSize: '2em', color: 'white'}}/>
-            </ul>
+            <li>
+            <SocialIcon icon={faUpwork}/>
+            </li>
         </Link>
         </ul>
         </div>
