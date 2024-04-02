22e64795c1041ce8084f10d07b481e17fe43d5af
Borislav Dukov <borislav.dukov@icloud.com>
2024-04-02 20:25:15 +0300

    edit banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -14,9 +14,9 @@ export default function Banner() {
             <p id='name-info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, laboriosam! </p>
         </div>
         <div className="banner-image">
-            <div className='img-container' id="image1">
+            {/* <div className='img-container' id="image1">
             <img src="./dukov.png" alt=""/>
-            </div>
+            </div> */}
             <div className='img-container' id="image2">
             <img src="./dukov1.png" alt=""/>
             </div>
