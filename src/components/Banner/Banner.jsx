b562d7505b66455aadd012cf6f3cc3f64fe88a00
Borislav Dukov <borislav.dukov@icloud.com>
2024-04-07 20:51:46 +0300

    create banner Responsive design

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,18 @@
 import './Banner.css'
 
 export default function Banner() {
-    const codeStr = '< coder >'
+    const codeStr = '< code >'
     return(
         <>
         <div className="banner">
 
-    <div className="hero-banner">
+    <div className="hero-banner">  
     <div className="container">
+
         <div className="banner-info">
             <h1 id="name">Borislav Dukov</h1>
             <h2 className="red-text">Web Developer</h2>
-            <p id='name-info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, laboriosam! </p>
+            <p id='name-info'> </p>
         </div>
         <div className="banner-image">
             {/* <div className='img-container' id="image1">
@@ -23,8 +24,9 @@ export default function Banner() {
         </div>
         <div className="banner-info2">
             <h1>{codeStr}</h1>
-            <h1>designer</h1>
+            <h1>design</h1>
         </div>
+
     </div>
 </div>
 </div>
