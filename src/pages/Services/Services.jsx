a8ba13bf9e092cebc6ad7c02ae1b7a15ea35eaba
Borislav Dukov <borislav.dukov@icloud.com>
2024-04-09 23:29:36 +0300

    edit services page head

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -6,9 +6,11 @@ export default function Services() {
     return(
         <>
         <div className="services-page">
+            <div className="services-banner">
             <div className="container">
                 <h2>I'm currently working on this page...</h2>
                 <img src="./Bdukov.png" alt="" />
+            </div>
             </div>
         </div>
         </>
