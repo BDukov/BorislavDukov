ee16727c5a108400838aa909fc7eee2153e2550b
Borislav Dukov <borislav.dukov@icloud.com>
2024-04-14 13:55:12 +0300

    add motion effects to Home Page
    Add Framer Motion to banner and projects components

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,11 @@
+import { motion } from 'framer-motion';
+
 import './Banner.css'
+
 
 export default function Banner() {
     const codeStr = '< code >'
+
     return(
         <>
         <div className="banner">
@@ -9,23 +13,35 @@ export default function Banner() {
     <div className="hero-banner">  
     <div className="container">
 
-        <div className="banner-info">
+        <motion.div 
+        initial={{ opacity: 0, x: -175 }}
+        animate={{ opacity:1, x: 0 }}
+        transition={{ duration: 450, delay: 0.5, type: 'spring', stiffness: 120 }}
+        className="banner-info">
             <h1 id="name">Borislav Dukov</h1>
             <h2 className="red-text">Web Developer</h2>
             <p id='name-info'> </p>
-        </div>
-        <div className="banner-image">
+        </motion.div>
+        <motion.div 
+        initial={{ opacity: 0, y: 175 }}
+        animate={{ opacity:1, y: 0 }}
+        transition={{ duration: 450, delay: 0.5, type: 'spring', stiffness: 120 }}
+        className="banner-image">
             {/* <div className='img-container' id="image1">
             <img src="./dukov.png" alt=""/>
             </div> */}
             <div className='img-container' id="image2">
             <img src="./dukov.png" alt=""/>
             </div>
-        </div>
-        <div className="banner-info2">
+        </motion.div>
+        <motion.div 
+                initial={{ opacity: 0, x: 175 }}
+                animate={{ opacity:1, x: 0 }}
+                transition={{ duration: 450, delay: 0.5, type: 'spring', stiffness: 120 }}
+        className="banner-info2">
             <h1>{codeStr}</h1>
             <h1>design</h1>
-        </div>
+        </motion.div>
 
     </div>
 </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,40 +1,75 @@
-import './Projects.css'
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
+
+import "./Projects.css";
 
 export default function Projects() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
-    return(
-        <>
-        <div className="projects">
+  return (
+    <>
+      <div className="projects">
+        <motion.p
+          whileHover={{ scale: 1.1 }}
+          style={{
+            transform: isInView ? "none" : "translateY(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+          id="headline"
+        >
+          Some of my lastest work
+        </motion.p>
 
-            <p id='headline'>Some of my lastest work</p>
-
-            <div className="container">
-
-                <article>
-                    <div className="image-container">
-                    <img src="./Projects/dicheva-home.png" alt="" />
-                    </div>
-                    <h3>Nadezhda Dicheva</h3>
-                    <p>Portfolio Website</p>
-                </article>
-
-                <article>
-                    <div className="image-container">
-                    <img src="./Projects/honey-app.png" alt="" />
-                    </div>
-                    <h3>Honey</h3>
-                    <p>Blog</p>
-                </article>
-
-                <article>
-                    <div className="image-container">
-                    <img src="./Projects/Plan B - Contacts.png" alt="" />
-                    </div>
-                    <h3>PlanB Web Services</h3>
-                    <p>Portfolio Website</p>
-                </article>
-
+        <div className="container">
+          <motion.article
+            ref={ref}
+            whileHover={{ scale: 1.1 }}
+            style={{
+              transform: isInView ? "none" : "translateY(200px)",
+              opacity: isInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            }}
+          >
+            <div className="image-container">
+              <img src="./Projects/dicheva-home.png" alt="" />
             </div>
-            </div></>
-    );
+            <h3>Nadezhda Dicheva</h3>
+            <p>Portfolio Website</p>
+          </motion.article>
+
+          <motion.article
+            whileHover={{ scale: 1.1 }}
+            style={{
+              transform: isInView ? "none" : "translateY(200px)",
+              opacity: isInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.7s",
+            }}
+          >
+            <div className="image-container">
+              <img src="./Projects/honey-app.png" alt="" />
+            </div>
+            <h3>Honey</h3>
+            <p>Blog</p>
+          </motion.article>
+
+          <motion.article
+            whileHover={{ scale: 1.1 }}
+            style={{
+              transform: isInView ? "none" : "translateY(200px)",
+              opacity: isInView ? 1 : 0,
+              transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.9s",
+            }}
+          >
+            <div className="image-container">
+              <img src="./Projects/Plan B - Contacts.png" alt="" />
+            </div>
+            <h3>PlanB Web Services</h3>
+            <p>Portfolio Website</p>
+          </motion.article>
+        </div>
+      </div>
+    </>
+  );
 }
